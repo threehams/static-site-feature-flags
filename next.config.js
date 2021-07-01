cadf2e64acc2f6ab1799bf605becc78eaaf2c8da
David Edmondson <threehams@gmail.com>
2021-06-30 22:08:59 -0700

    repro beforeFiles bug

--- next.config.js
+++ next.config.js
@@ -3,56 +3,6 @@ module.exports = {
     return {
       beforeFiles: [
         {
-          source: "/:makeSlug(acura|honda)",
-          destination: "/:host/overview/:makeSlug/challenger",
-          has: [
-            {
-              type: "host",
-              value: "(?<host>.*)",
-            },
-            {
-              type: "cookie",
-              key: "critical_flags",
-              value: "feature1challenger",
-            },
-          ],
-        },
-        {
-          source: "/:makeSlug(acura|honda)",
-          destination: "/:host/overview/:makeSlug/control",
-          has: [
-            {
-              type: "host",
-              value: "(?<host>.*)",
-            },
-            {
-              type: "cookie",
-              key: "critical_flags",
-              value: "feature1control",
-            },
-          ],
-        },
-        {
-          source: "/:condition(new|used)-cars-for-sale",
-          destination: "/:host/marketplace/:condition",
-          has: [
-            {
-              type: "host",
-              value: "(?<host>.*)",
-            },
-          ],
-        },
-        {
-          source: "/:car-:paymentType(loan|lease)-calculator",
-          destination: "/:host/calculator/:paymentType",
-          has: [
-            {
-              type: "host",
-              value: "(?<host>.*)",
-            },
-          ],
-        },
-        {
           source: "/",
           destination: "/:host/home/default",
           has: [
--- pages/[host]/home/default/index.tsx
+++ pages/[host]/home/default/index.tsx
@@ -3,10 +3,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { delay } from "../../../../lib/delay";
 
-type HomeProps = {
-  locale: string;
-};
-export default function Home({ locale }: HomeProps) {
+type HomeProps = {};
+export default function Home({}: HomeProps) {
   return (
     <div>
       <Head>
@@ -15,59 +13,16 @@ export default function Home({ locale }: HomeProps) {
       </Head>
 
       <main>
-        <h1 className="mb-4">
-          Welcome to Partner{" "}
-          <a href="https://nextjs.org">Next.js on partner: {locale}</a>
-        </h1>
-
-        <p>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div>
-          <Link href="/legal/privacy">
-            <a>Privacy &rarr;</a>
-          </Link>
-
-          <Link href="/car-loan-calculator">
-            <a>Loan Calculator &rarr;</a>
-          </Link>
-
-          <Link href="/car-lease-calculator">
-            <a>Lease Calculator &rarr;</a>
-          </Link>
-
-          <Link href="/honda">
-            <a>Honda &rarr;</a>
-          </Link>
-
-          <Link href="/honda/deals">
-            <a>Honda Deals &rarr;</a>
-          </Link>
-
-          <Link href="/honda/civic">
-            <a>Honda Civic &rarr;</a>
-          </Link>
-
-          <Link href="/honda/civic/reviews">
-            <a>Honda Reviews &rarr;</a>
-          </Link>
-
-          <Link href="/docs">
-            <a>Documentation &rarr;</a>
-          </Link>
-        </div>
+        <h1 className="mb-4">Home</h1>
       </main>
     </div>
   );
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async ({ params }) => {
-  await delay(1000, "partner home");
   return {
     props: {
       title: "hi",
-      locale: params?.host ?? "",
     },
     revalidate: 60,
   };
